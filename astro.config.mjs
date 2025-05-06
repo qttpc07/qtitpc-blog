c5f2830057638800216f42c1394d0f9aea6c4213
qttpc07 <21107.qt.vn@gmail.com>
2025-05-06 22:06:50 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,8 +20,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://qttpc07.github.io/",
+	base: "/qtitpc-blog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
